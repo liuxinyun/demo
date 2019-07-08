0726573a26a462244a6ea949c3106fc08dcc3fba
liuxinyun <liuxinyun@koolearn.com>
2019-07-08 21:05:10 +0800

    增加spring-boot-demo

---
+++ spring-boot-demo/src/main/java/com/lxy/springbootdemo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.lxy.springbootdemo.controller;
+
+import com.lxy.demo.common.ResultData;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 刘新运
+ * @date 2019/7/8 20:47
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public ResultData get(@RequestParam(required = false) String name){
+        String res = "Hello";
+        if (null != name) {
+            res += "," + name;
+        }
+        return ResultData.successed(res + "!");
+    }
+}
